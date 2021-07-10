bc02fe667397d556d13b748383e76c60e76b55c3
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-07-10 11:15:28 +0530

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,2 +1,17 @@
 @echo off
 
+node -v
+
+npm -v
+
+npm install discord.js@12.5.3
+
+npm install quick.db@7.1.3
+
+npm install reconlx@1.1.3
+
+npm install discord-reply discord-buttons@4.0.0
+
+npm install parse-ms moment canvas-senpai weather-js node-fetch common-tags
+
+npm install
