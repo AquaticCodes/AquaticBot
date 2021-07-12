3cce495392c125cc8467283b887facf9e4d481d4
AquaticCodes <85302758+AquaticCodes@users.noreply.github.com>
2021-07-12 09:51:18 +0530

    Create members.js

---
+++ commands/settings/members.js
@@ -0,0 +1,21 @@
+/*
+
+This file concentrates on using database to update
+the guild configuration settings on members!
+
+thus it means, it works on sending messages when a 
+member joins and when he leaves :)
+
+*/
+
+module.exports = {
+name: "members",
+guild: true,
+authorPermission: ["MANAGE_GUILD", "MANAGE_CHANNELS", "MANAGE_MESSAGES"],
+run: async (client, message, args) => {
+
+// code goes here :)
+
+
+},
+};
